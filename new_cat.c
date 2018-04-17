d163469f6b511611bcf002b70ff67c5db1e7890e
Miller <millera16@debian-9.3.0-template>
2018-04-16 20:38:37 -0700

    Added conditions for one filename string and too many string arguements. Currently needs adjustment for the standard input string buffer size.

--- new_cat.c
+++ new_cat.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include<stdlib.h>
 //new_cat.c
 //This is a simple cat replacement program that will print the ocntents of a
 //text file or input string to stdout
@@ -8,17 +8,37 @@
 
 int main(int argc, char  *argv[])
 {
+	FILE *rfile;
+	char c;
 	char output[100];
-	printf("%d,", i);
 	if (argc == 1)//condition for no filename passed in
 	{
 		scanf("%s", output);//Gets output string from user
 		printf(output);//prints output to console
 	}
-	else if (argc == 2)
+	else if (argc == 2)//condition for one filename passed in
 	{
-	
+		rfile = fopen(argv[1], "r"); //opens file specified by string
+		if (rfile == NULL)//file does not exist, fail gracefully
+		{
+			printf("Error, specified file does not exist\n");
+			return 0;
+		}
+		else //file does exist, print it to stdout
+		{
+			c = fgetc(rfile);//Prints text file char by char
+			while (c != EOF)
+			{
+			printf("%c", c);
+			c = fgetc(rfile);
+			}
+		
+		}
+		fclose(rfile);//closes file
 	}
-
+	else //condition for more than one file. Will print failure message
+	{
+		printf("Error. Only one filename is accepted\n");
+	}
 	return 0;
 }
