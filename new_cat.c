22d572c1c6c05fa9f186f73b9d9918e674556a60
Miller <millera16@debian-9.3.0-template>
2018-04-16 19:49:22 -0700

    Added new_cat.c, a replacement cat function. Currently only has user input from stdin condition coded.

---
+++ new_cat.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+//new_cat.c
+//This is a simple cat replacement program that will print the ocntents of a
+//text file or input string to stdout
+
+
+
+int main(int argc, char  *argv[])
+{
+	char output[100];
+	printf("%d,", i);
+	if (argc == 1)//condition for no filename passed in
+	{
+		scanf("%s", output);//Gets output string from user
+		printf(output);//prints output to console
+	}
+	else if (argc == 2)
+	{
+	
+	}
+
+	return 0;
+}
