2afeb92e67a1a33a10eaa4f471fc625d4212d604
Miller <millera16@debian-9.3.0-template>
2018-04-19 19:34:58 -0700

    Corrected arguement detection conditions, added full functionality for calling tail with a file arguement. Still needs comments and checks to make sure the arguement passed in is a file that can open.

--- new_tail.c
+++ new_tail.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 //new_tail.c
 //This will print the last 5 lines of a file or use standard input if no file is specified
@@ -13,28 +14,78 @@
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	bool n = 0;
+	int n = 0;
+	FILE *rf;
+	int file = 1;
 
-	char path[100];
 
 	int lines = 5; //Default line number. Will be adjusted if -n flag is found
-	if (argc > 4)//Condition for more than 3 string arguements passed in
+	if (argc > 4)//Condition for more than 4 string arguements passed in
 	{
-		printf("Error, too many arguements passed in\n");
+		printf("Error. Please enter the appropriate number of arguements.\n");
 		return 0;
 
 	}
 	for (i = 0; i < argc; i++)
 	{
-		if (argv[i] == "-n")//loops through arguements to see if a -n flag was passed in
+		if (strcmp(argv[i], "-n") == 0)//loops through arguements to see if a -n flag was passed in
+		{
 			n = 1;
+			lines = atoi(argv[i+1]);//Gets new line number
+			if ( i == 2 && argc == 4)//These are checks to find thew file in the arguiements when the user pases in the -n flag
+				rf = fopen(argv[3], "r");
+			else if (i == 1 && argc == 4)
+				rf = fopen(argv[3], "r");
+			else
+				file = 0;
+
+
+		}
 	}
-	if  (n == 1)//will be true when the -n flag is found, searches for the line number 
+	if  (n == 0 && argc > 2)//will be true when there are an appropirate number of parameters, but the parameters are invalid
 	{
-		lines = atoi(argv[i+1]); //Gets new line number
+		printf("Error. Please input only a file with the optional -n flag followed by a line number.\n");
+		return 0;
 	}
-	
+	if (n == 0 && argc == 2)//condition for no -n flag but file input
+	{
+		rf = fopen(argv[1], "r");
+	}
+	if (argc == 1 || file == 0) //condition for no file input, get from stdin
+	{
+				
+
+		return 0;
+	}
+	else //condition for file input
+	{
+		int count = 0;
+		char c;
+		fflush(stdout);
+		fseek(rf, 0, SEEK_END);
+		if ( fgetc(rf) == '\n')
+		{
+		count++;
+		}
+		while (count < lines)
+		{
+			fseek(rf, -2, SEEK_CUR);
+			if (fgetc(rf) == '\n')
+			{
+				count++;
+			}
+		
+		}
+		c = fgetc(rf);
+		while(c != EOF)
+		{
+			printf("%c", c);
+			c = fgetc(rf);
+		
+		}
+	}
 
 
+	fclose(rf);
 	return 0;
 }
