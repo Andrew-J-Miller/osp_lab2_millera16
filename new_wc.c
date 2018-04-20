344d82360092d3783698348f8328f7e5e78685cf
Miller <millera16@debian-9.3.0-template>
2018-04-19 22:56:15 -0700

    Final version of new_wc.c. Added comments and condition for user input with unlimited input buffer and newline support.

--- new_wc.c
+++ new_wc.c
@@ -9,6 +9,18 @@
 //only one flag can be passed in
 
 
+void strAppend(char c, char **string)
+{
+	size_t len = strlen(*string);
+	char *str2 = malloc(len + 1 + 1);
+	strcpy(str2, *string);
+	str2[len] = c;
+	string[len+1] = '\0';
+	*string = malloc(len + 1 + 1);
+	strcpy(*string, str2);
+
+}
+
 
 
 int main(int argc, char *argv[])
@@ -41,27 +53,80 @@ int main(int argc, char *argv[])
 		}
 		if (strcmp(argv[i], "-c") != 0 && strcmp(argv[i], "-l") != 0 && i != 0)
 		{
-			file = i;
+			file = i; //This is the position makrer for file in argv
 		}
 	}
 
 	if (file == 0)//condition for no file, get from stdin
 	{
+		char *str = "";
+		char c2;
+		//Loop that gets user input until ctrl d is pressed
+		while(c2 != EOF)
+		{
+			c2 = getchar();
+			if (c2 != EOF)
+			{
+				strAppend(c2, &str);//constantly appends new char to string
+			}
+		}
+
+		size_t len = strlen(str);
+		int j = 0;
+		//loops through string to find words/chars/lines
+		for (j = 0; j < len; j++)
+		{
+			chars++;
+			if (str[j] == ' ')
+			{
+				words++;
+			}
+			if (str[j] == '\n')
+			{
+				lines++;
+				words++;
+			}
+		
+		}
 	
+
+		if (chars != 0 ) //if there are characters but no spaces there is still one word
+		{
+			words++;
+		}
+		if (lines == 0 && words != 0)//if there are words but no newline there is still one line
+		{
+			lines++;
+		}
+
+		//selects appropriate response based on flag input
+		if (l == 1)
+		{
+			printf("\n%d %d\n", lines, words);
+		}
+		else if (c == 1)
+			printf("\n%d %d\n", words, chars);
+		else
+			printf("\n%d\n", words);
+
+
+		fflush(stdout);
+		fflush(stdin);
+		return 0;
 	}
 	else //condition for file was passed in
 	{
 	
 		FILE *rf;
 		char ch;
-
+		//opens file and makes sure it exists
 		rf = fopen(argv[file], "r");
 		if (rf == NULL)
 		{
 			printf("Error. File does not exist");
 			return 0;
 		}
-		
+		//Loops through file tracking words, lines and chars
 		ch = fgetc(rf);
 		while (ch != EOF)
 		{
@@ -73,17 +138,20 @@ int main(int argc, char *argv[])
 			if (ch == '\n')
 			{
 				lines++;
+				words++;
 			}
 			ch = fgetc(rf);
 		}
-		if (chars != 0)
+		if (chars != 0)//if there are characters but no space there is still a word
 		{
 			words++;
 		}
-		if (lines == 0 && words != 0)
+		if (lines == 0 && words != 0)//if there are words but no newline there is still a line
 		{
 			lines++;
 		}
+
+		//Selects appropriate response based on flags in argv
 		if (c == 1)
 		{
 			printf("%d %d\n", words, chars);
